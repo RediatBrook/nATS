7d9bbfa378b517478d5eaea0c9ea6d2d7ec43afe
Rediat <rbshamsu@crimson.ua.edu>
2023-06-19 23:45:50 -0700

    Separated frontend

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
 from flask import Flask
-
+from flask import send_file
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return send_file('./frontend/index.html')
 
 @app.route('/about')
 def about():
     return 'About'
+
+# Implement Adding a Job to the Database
+@app.route('/jobs/add')
+def add_job():
+    return 'Add Job'
+
+#implement A Resume to 
+@app.route('/jobs/<int:id>/submit')
+def submit_resume(id):
+    return str(id)
