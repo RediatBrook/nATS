9cd847835ee3caaa478bda613e34647f40e81068
benLBrook <benealbrook@gmail.com>
2023-06-20 22:25:35 -0700

    Added S3 Upload and Download For Resumes

--- api/index.py
+++ api/index.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import send_file, request
+import os 
+import boto3 
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,3 +22,47 @@ def add_job(description):
 @app.route('/jobs/<int:id>/submit')
 def submit_resume(id):
     return id
+
+@app.route("/uploadToS3/")
+def uploadToS3():
+    text = request.args.get("text")
+    bucket_name = 'textfiledata'
+    person = str(request.args.get("person"))
+    key = 'testingfolder/' + person + '.txt'
+
+    # Create an S3 client using the system variables
+    session = boto3.Session()
+
+    # Create the S3 client
+    s3_client = session.client('s3')
+
+    # Upload text to S3
+    response = s3_client.put_object(
+        Bucket=bucket_name,
+        Key=key,
+        Body=text.encode('utf-8')
+    )
+
+    return "Uploaded text to S3"
+
+@app.route("/downloadFromS3/")
+def downloadFromS3():
+    bucket_name = 'textfiledata'
+    person = str(request.args.get("person"))
+    key = 'testingfolder/' + person + '.txt'
+
+    # Create an S3 client using the system variables
+    session = boto3.Session()
+
+    # Create the S3 client
+    s3_client = session.client('s3')
+
+    # Download text from S3
+    response = s3_client.get_object(
+        Bucket=bucket_name,
+        Key=key
+    )
+
+    text = response['Body'].read().decode('utf-8')
+
+    return "Downloaded text from S3: " + text
