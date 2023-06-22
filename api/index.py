e0d18abedadf6a3b33916e9b600dcf5bf8ecb552
tsionshamsu <70413034+tsionshamsu@users.noreply.github.com>
2023-06-22 01:36:42 -0700

    Visenya added the embedding features from the openai api

--- api/index.py
+++ api/index.py
@@ -2,7 +2,12 @@ from flask import Flask
 from flask import render_template
 from flask import send_file, request
 import os 
+import openai
 import boto3 
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -70,3 +75,13 @@ def downloadFromS3():
 @app.route("/applicants")
 def applicants():
     return render_template('applicants.html')
+
+@app.route("/getEmbedding/<text>")
+def getEmbeding(text):
+    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+    def get_embedding(text: str, model="text-embedding-ada-002") -> list[float]:
+        return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
+    embedding = get_embedding(text, model="text-embedding-ada-002")
+    #printing only for debugging purposes
+    print(embedding)
+    return embedding
